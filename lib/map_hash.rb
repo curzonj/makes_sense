e7c047e5d4fa32e0310821d49c1aab5e12c2951b
Jordan Curzon <curzonj@gmail.com>
2009-12-16 15:08:37 -0800

    Added map_hash

---
+++ lib/map_hash.rb
@@ -0,0 +1,19 @@
+module Enumerable
+  def map_hash(&block)
+    hash = {}
+
+    list = map(&block)
+
+    list.each do |result|
+      if result.is_a?(Array)
+        hash[result.first] = result.last
+      elsif result.is_a?(Hash)
+        hash.merge!(result)
+      else
+        hash[result] = result
+      end
+    end
+    
+    hash
+  end
+end
